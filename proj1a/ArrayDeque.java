b1e9a2fa6f3e51b6d17ef4348d2b373de47326a4
Crimsonlm <kjxtiger@126.com>
2021-07-07 18:45:31 +0800

    ArrayDeque2

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -13,6 +13,9 @@ public class ArrayDeque<T> {
         if (size == items.length) {
             resize();
         }
+        if(head<0){
+            head=head+ items.length;
+        }
         items[head] = i;
         head = head - 1;
         size += 1;
@@ -23,13 +26,16 @@ public class ArrayDeque<T> {
         if (size == items.length) {
             resize();
         }
+        if(tail==items.length){
+            tail=0;
+        }
         items[tail]=i;
         tail=tail+1;
         size+=1;
 
 
     }
-    public void resize(){
+    private void resize(){
         T[]a =(T[])new Object[size*2];
         System.arraycopy(items,head+1,a,0,items.length-head-1);
         System.arraycopy(items,0,a,items.length-head-1,tail);
@@ -56,13 +62,24 @@ public class ArrayDeque<T> {
         }
     }
     public T removeFirst(){
-        T result=items[head+1];
-        items[head+1]=null;
+        head=(head+1)% items.length;
+        T result=items[head];
+        items[head]=null;
+        if(size>0){
+            size=size-1;
+        }
         return result;
     }
     public T removeLast(){
+        if(tail==0){
+            tail=tail+ items.length;
+        }
         T result=items[tail-1];
         items[tail-1]=null;
+        tail=tail-1;
+        if(size>0){
+            size=size-1;
+        }
         return result;
     }
 
@@ -72,6 +89,63 @@ public class ArrayDeque<T> {
         }else{
             return items[index-(items.length-head-1)];
         }
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque t =new ArrayDeque();
+        t.addLast(1);
+        t.addFirst(3);
+        t.addLast(5);
+        t.addFirst(2);
+        t.addLast(7);
+        t.addFirst(4);
+        t.addLast(8);
+        t.addFirst(9);
+        t.addLast(0);
+        t.addFirst(6);
+        t.addLast(5);
+        t.addFirst(7);
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.removeLast();
+        t.addLast(1);
+        t.addLast(2);
+        t.addLast(3);
+        t.addLast(4);
+        t.addLast(5);
+        t.addLast(6);
+        t.addLast(7);
+        t.addLast(8);
+        t.addLast(9);
+        t.addLast(10);
+        t.addLast(11);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
